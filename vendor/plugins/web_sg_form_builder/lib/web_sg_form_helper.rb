886fcdca9290ff9c050125f344b0a957a9cf0119
choonkeat <choonkeat>
2008-03-05 15:36:30 +0000

    ....

--- vendor/plugins/web_sg_form_builder/lib/web_sg_form_helper.rb
+++ vendor/plugins/web_sg_form_builder/lib/web_sg_form_helper.rb
@@ -1,7 +1,15 @@
 module WebSgFormHelper
   def form_for(name, *args, &proc)
-    args.push({}) unless args.last.respond_to?(:merge)
-    args.last.merge! :builder => WebSgFormBuilder
+    unless args.empty?
+      args.push({})  unless args.last.respond_to?(:merge)
+      args.last.merge! :builder => WebSgFormBuilder
+    end
+    super
+  end
+  
+  # Take effect if Rails 2
+  def apply_form_for_options!(object_or_array, options)
+    options.merge! :builder => WebSgFormBuilder
     super
   end
 end
