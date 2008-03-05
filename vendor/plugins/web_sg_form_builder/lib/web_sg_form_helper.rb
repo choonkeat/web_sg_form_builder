d4e1b0b727891dd192ccfb0863eb24ab8a8a8436
choonkeat <choonkeat>
2008-03-05 07:10:04 +0000

    sorry. now its really compatible with Rails 1.2 & Rails 2

--- vendor/plugins/web_sg_form_builder/lib/web_sg_form_helper.rb
+++ vendor/plugins/web_sg_form_builder/lib/web_sg_form_helper.rb
@@ -1,6 +1,7 @@
 module WebSgFormHelper
-  def form_for(name, object = nil, options = {}, &proc)
-    options = (options || {}).merge(:builder => WebSgFormBuilder)
+  def form_for(name, *args, &proc)
+    args.push({}) unless args.last.respond_to?(:merge)
+    args.last.merge! :builder => WebSgFormBuilder
     super
   end
 end
