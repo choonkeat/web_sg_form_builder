9d33acceaba023f6439a461ee042a25ffb96dd24
choonkeat <choonkeat@gmail.com>
2008-09-25 23:47:28 -0700

    adding autogenerated CSS classes for labels and hints as well

--- vendor/plugins/web_sg_form_builder/lib/web_sg_form_builder.rb
+++ vendor/plugins/web_sg_form_builder/lib/web_sg_form_builder.rb
@@ -33,7 +33,7 @@ class WebSgFormBuilder
   
   def dd(label, hint = nil, &proc)
     if proc
-      concat("<dt><label>#{label.to_s}</label> <span>#{hint}</span></dt><dd>", proc.binding)
+      concat("<dt><label class=\"dd\">#{label.to_s}</label> <span class=\"dd\">#{hint}</span></dt><dd>", proc.binding)
       proc.call(self)
       concat("</dd>", proc.binding)
     else
@@ -45,17 +45,19 @@ class WebSgFormBuilder
   
   def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
     label = options.delete(:label) || method.to_s.humanize
+    field_id = CGI.escapeHTML([@builder.object_name, method].join('_').downcase)
     @builder.check_box(method, options, checked_value, unchecked_value) +
     " " +
-    "<label for=\"#{CGI.escapeHTML([@builder.object_name, method].join('_').downcase)}\"" + 
+    "<label class=\"check_box #{field_id} #{options[:label_class]}\" for=\"#{field_id}\"" +
     ">#{label}</label>"
   end
   
   def radio_button(method, tag_value, options = {})
     label = options.delete(:label) || tag_value.to_s.humanize
+    field_id = CGI.escapeHTML([@builder.object_name, method, tag_value].join('_').downcase)
     @builder.radio_button(method, tag_value, options) +
     " " +
-    "<label for=\"#{CGI.escapeHTML([@builder.object_name, method, tag_value].join('_').downcase)}\"" + 
+    "<label class=\"radio_button #{field_id} #{options[:label_class]}\" for=\"#{field_id}\"" +
     ">#{label}</label>"
   end
   
@@ -72,10 +74,12 @@ class WebSgFormBuilder
         options = args.shift || {}
         # select has extra argument before options hash
         opts = options.kind_of?(Hash) ? options : args.first
-        label = opts.delete(:label) || method.to_s.humanize
-        hint  = opts.delete :hint
-        "<dt><label for=\"#{CGI.escapeHTML([@builder.object_name, method].join('_').downcase)}\"" + 
-        ">#{label}</label> <span>#{hint}</span></dt><dd" + 
+        label = (opts.respond_to?(:delete) ? opts.delete(:label) : nil) || method.to_s.humanize
+        hint  = (opts.respond_to?(:delete) ? opts.delete(:hint) : nil)
+        label_class = (opts.respond_to?(:delete) ? opts.delete(:label_class) : nil)
+        field_id = CGI.escapeHTML([@builder.object_name, method].join('_').downcase)
+        "<dt><label class=\"#{input_field} #{field_id} #{label_class}\" for=\"#{field_id}\"" +
+        ">#{label}</label> <span class=\"#{input_field} #{field_id} #{label_class}\">#{hint}</span></dt><dd" +
         ">#{@builder.send(input_field, method, options, *args)}</dd>"
       end
     end
